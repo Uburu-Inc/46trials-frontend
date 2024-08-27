31bafe7ff85f2f4d6593f554091cdf633cc2be01
Williams Alex <godswillwilliams9@gmail.com>
2024-08-27 08:31:44 +0100

    Chore: Made minor updates to 46trials

--- src/app/dashboard/pages/projects/[id]/[slug]/components/index.tsx
+++ src/app/dashboard/pages/projects/[id]/[slug]/components/index.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
+
 import { Tab } from "@/app/components/reusable-components/tab";
+import { Loader } from "@/app/components/reusable-components/loader";
 import { HeaderTitle } from "@/app/dashboard/components/header-title";
 import { TagsScrollView } from "@/app/dashboard/components/tags-scroll-view";
 import { ProjectSummaryComponent } from "./project-summary";
 import { Response } from "./response";
-import { useFetch46TrialById } from "@/app/dashboard/hooks/projects/46trials/fetch-trial-by-id"
+import { useFetch46TrialById } from "@/app/dashboard/hooks/projects/46trials/fetch-trial-by-id";
 
 interface Props {
   id: string;
-  slug: string
+  slug: string;
 }
 
 export function ViewProjectSummary({ id, slug }: Props) {
-  const { data } = useFetch46TrialById(Number(id))
+  const { loading, data } = useFetch46TrialById(Number(id));
+  const projectName = slug.replaceAll("-", " ");
   return (
     <div className="px-10">
-      <HeaderTitle
-        title={["Projects", slug.replaceAll("-"," ")]}
-        header="Ebola Reasearch"
-      />
+      <HeaderTitle title={["Projects", projectName]} header={projectName} />
       <div className="px-1">
         {/* <TagsScrollView
           className="mt-3 w-[70rem]"
@@ -33,38 +33,49 @@ export function ViewProjectSummary({ id, slug }: Props) {
             "Test",
           ]}
         /> */}
-
-        <Tab
-          className="mt-5"
-          current="project-summary"
-          title={[
-            { value: "project-summary", text: "Project Summary" },
-            { value: "response", text: "Response" },
-          ]}
-          interfaceItems={[
-            {
-              value: "project-summary",
-              component: (
-                <ProjectSummaryComponent
-                  sampleSize={data?.sample_size ?? ""}
-                  budget={data?.budget ?? ""}
-                  startDate={new Date(data?.start_date ?? "").toLocaleDateString()}
-                  endDate={new Date(data?.end_date ?? "").toLocaleDateString()}
-                />
-              ),
-            },
-            {
-              value: "response",
-              component: (
-                <Response
-                  name={data?.name ?? ""}
-                  date={data?.start_date ?? ""}
-                  list={data?.trial_files ?? []}
-                />
-              ),
-            },
-          ]}
-        />
+        {loading ? (
+          <div className="flex justify-center mt-10">
+            <Loader className="h-14 w-14" />
+          </div>
+        ) : (
+          <>
+            <Tab
+              className="mt-5"
+              current="project-summary"
+              title={[
+                { value: "project-summary", text: "Project Summary" },
+                { value: "response", text: "Response" },
+              ]}
+              interfaceItems={[
+                {
+                  value: "project-summary",
+                  component: (
+                    <ProjectSummaryComponent
+                      sampleSize={data?.sample_size ?? ""}
+                      budget={data?.budget ?? ""}
+                      startDate={new Date(
+                        data?.start_date ?? ""
+                      ).toLocaleDateString()}
+                      endDate={new Date(
+                        data?.end_date ?? ""
+                      ).toLocaleDateString()}
+                    />
+                  ),
+                },
+                {
+                  value: "response",
+                  component: (
+                    <Response
+                      name={data?.name ?? ""}
+                      date={data?.start_date ?? ""}
+                      list={data?.trial_files ?? []}
+                    />
+                  ),
+                },
+              ]}
+            />
+          </>
+        )}
       </div>
     </div>
   );
--- src/app/dashboard/pages/projects/components/46trials.tsx
+++ src/app/dashboard/pages/projects/components/46trials.tsx
@@ -4,10 +4,11 @@ import { cn } from "@/lib/tailwind/cn";
 import { useState } from "react";
 import { DashboardCard } from "../../../components/card";
 import { useFetch46Trials } from "../../../hooks/projects/46trials/fetch-trials";
+import { Loader } from "@/app/components/reusable-components/loader";
 
 export function FortySixTrials() {
-  const { data } = useFetch46Trials();
-  const [status, setStatus] = useState(0);
+  const { loading, data } = useFetch46Trials();
+  const [status] = useState(0);
   return (
     <section>
       {/* <div className="flex gap-3">
@@ -35,44 +36,50 @@ export function FortySixTrials() {
         </button>
       </div> */}
 
-      <div className="py-3">
-        {status === 0 && (
-          <div className="flex justify-between flex-wrap">
-            {data?.results.map((item, index) => (
-              <div className="w-[31%]" key={index}>
-                <DashboardCard
-                  className="mt-7"
-                  title={`Created on ${new Date(
-                    item.created_at
-                  ).toDateString()}`}
-                  content={item.name}
-                  path={`/dashboard/pages/projects/${
-                    item.id
-                  }/${item.name.replaceAll(" ", "-")}`}
-                />
-              </div>
-            ))}
-          </div>
-        )}
-        {status === 1 && (
-          <div className="flex justify-between flex-wrap">
-            {data?.results.map((item, index) => (
-              <div className="w-[31%]" key={index}>
-                <DashboardCard
-                  className="mt-7"
-                  title={`Created on ${new Date(
-                    item.created_at
-                  ).toDateString()}`}
-                  content={item.name}
-                  path={`/dashboard/pages/projects/${
-                    item.id
-                  }/${item.name.replaceAll(" ", "-").toLowerCase()}`}
-                />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+      {loading ? (
+        <div className="flex justify-center">
+          <Loader className="h-14 w-14" />
+        </div>
+      ) : (
+        <div className="py-3">
+          {status === 0 && (
+            <div className="flex justify-between flex-wrap">
+              {data?.results.map((item, index) => (
+                <div className="w-[31%]" key={index}>
+                  <DashboardCard
+                    className="mt-7"
+                    title={`Created on ${new Date(
+                      item.created_at
+                    ).toDateString()}`}
+                    content={item.name}
+                    path={`/dashboard/pages/projects/${
+                      item.id
+                    }/${item.name.replaceAll(" ", "-")}`}
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+          {status === 1 && (
+            <div className="flex justify-between flex-wrap">
+              {data?.results.map((item, index) => (
+                <div className="w-[31%]" key={index}>
+                  <DashboardCard
+                    className="mt-7"
+                    title={`Created on ${new Date(
+                      item.created_at
+                    ).toDateString()}`}
+                    content={item.name}
+                    path={`/dashboard/pages/projects/${item.id}/${item.name
+                      .replaceAll(" ", "-")
+                      .toLowerCase()}`}
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
     </section>
   );
 }
